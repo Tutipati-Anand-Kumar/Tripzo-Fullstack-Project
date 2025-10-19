1ed266de68b1b4a8aadc0af01fa9de98fc724d86
= <=>
2025-10-19 20:02:41 +0530

    some modifications done

--- backend_tripzo/routes/authRoutes.js
+++ backend_tripzo/routes/authRoutes.js
@@ -8,6 +8,6 @@ router.post("/request-otp", authController.requestOTP);
 router.post("/verify-otp", authController.verifyOTP);
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.get("/me", authMiddleware, authController.getMe);
+router.get("/me", authController.getMe);
 
 module.exports = router;
